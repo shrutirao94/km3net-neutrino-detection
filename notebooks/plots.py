e01317c5663aef6d28f34d115f002f551701b1d0
Shruti Rao <shrutirao94@gmail.com>
2020-06-19 11:38:58 +0200

    Graphs
    1. PMT Layout

--- notebooks/plots.py
+++ notebooks/plots.py
@@ -5,8 +5,15 @@ from mpl_toolkits import mplot3d
 
 data = pd.read_csv("../data/data.csv", index_col=None)
 
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.scatter3D(data.pos_x, data.pos_y, data.pos_z)
+
+# plt.show()
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter3D(data.pos_x, data.pos_y, data.pos_z)
-
+ax.scatter3D(data.dir_x, data.dir_y, data.dir_z)
+ax.set_title("Layout of PMTs used for generating data")
+plt.savefig("../assets/pmt_layout.png")
 plt.show()
